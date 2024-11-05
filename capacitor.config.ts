d6e3831cd31737406f38a86c16353039f45840d3
Sebastian R <s.rebolledos@duocuc.cl>
2024-11-05 20:05:23 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Seba_Rebolledo_007v',
+  webDir: 'www'
+};
+
+export default config;
